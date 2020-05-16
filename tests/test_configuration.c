57b6b6e7e174f46198a50e88b33a3004c1fe39a9
Vicente Obregon <desarrollogis@gmail.com>
2020-05-16 09:52:22 -0700

    test

--- tests/test_configuration.c
+++ tests/test_configuration.c
@@ -1,6 +1,10 @@
 #include "raylib.h"
 #include "../src/configuration.h"
 
+const int g_screenWidth = 0;
+const int g_screenHeight = 0;
+const int g_border = 0;
+
 int main(int argc, char* argv[]) {
 	struct dashboard* pDashboards = loadConfiguration(argc, argv);
 
