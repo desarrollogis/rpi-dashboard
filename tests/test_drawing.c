9f137490e517c6434a7dbab7b9aa45ff8a3ea843
Vicente Obregon <desarrollogis@gmail.com>
2020-05-17 20:43:34 -0700

    draw

---
+++ tests/test_drawing.c
@@ -0,0 +1,23 @@
+#include "raylib.h"
+#include "../src/drawing.h"
+
+int main(int argc, char* argv[]) {
+	struct dashboard* pDashboards = loadConfiguration(argc, argv);
+
+	if (pDashboards == 0) {
+		TraceLog(LOG_ERROR, "No dashboards.");
+	}
+
+	struct dashboard* pPointer = pDashboards;
+
+	InitWindow(g_width, g_height, "dashboard");
+	SetConfigFlags(FLAG_VSYNC_HINT);
+	SetTargetFPS(10);
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		drawDashboard(pPointer);
+		EndDrawing();
+	}
+	CloseWindow();
+	return 0;
+}
